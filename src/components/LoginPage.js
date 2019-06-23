e770d408444d7df70874219f151ba6178fef29db
Igor Dumencic <igordumencic2@gmail.com>
2019-06-23 13:28:19 +0200

    Styling the app

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -4,8 +4,12 @@ import PropTypes from 'prop-types'
 import { startLogin } from '../redux/actions/authActions'
 
 const LoginPage = ({ startLogin }) => (
-    <div>    
-        <button onClick={startLogin}>Sign Up</button>
+    <div id='login-page__wrapper'>
+        <div className='login-page__box'>
+            <h1 className='box__title'>Budgeter</h1>    
+            <p className='box__subtitle'>Let's get your expenses under control.</p>
+            <button className='box__btn' onClick={startLogin}>Sign up with Google</button>
+        </div>
     </div>
 )
 
